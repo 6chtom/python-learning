85d4794ab98108420a6f40f530115c6740612fde
6chtom <liuchenhui@tom.com>
2023-04-10 21:32:53 +0800

    Main

--- P4-变量和字符串（上）.py
+++ P4-变量和字符串（上）.py
@@ -1,2 +1,39 @@
 x = 3
 print(x)
+
+loveyou1314 = 1314
+
+# 520baby = 1314  不能以数字打头
+
+幸运数 = 588
+print(幸运数)
+
+name = "小甲鱼"
+print(name)
+
+
+#python支持直接交换数值
+a = 2
+b = 6
+a,b = b,a
+print(a,b)
+
+
+######################################
+#字符串表达有三种 Single quotes/ Double quotes/ Triple quotes
+
+#Single quotes
+print('I love China')
+
+#Double quotes
+print("I love China")
+
+# print('Let's Go')， 这里出错，只能用双引号
+# print('"Life is short, I need python"') 像这种名言一般用双引号，所以需要用单引号写程序
+# 如果两种都要使用的话，比如输出"Life is short, let's learning python"，怎么办？
+# 可以用转义字符进行转换。print('"Life is short, let\'s learning python"')，或print('\"Life is short, let\'s learning python\"')
+print('"Life is short, let\'s learning python"')
+print('\"Life is short, let\'s learning python\"')
+
+#####换行符\n
+print("I love python.\nI love fishc.")
